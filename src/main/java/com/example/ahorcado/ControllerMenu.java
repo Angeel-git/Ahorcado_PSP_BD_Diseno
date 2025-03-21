cf1dbfd137d11a5e268cc59c80f24e91246d753c
Angel <angelfernandezblanco16@gmail.com>
2025-03-21 23:17:08 +0100

    Consigo que el servidor se lance cuando el jugador cambia de la pantalla del menu al escenario del juego

--- src/main/java/com/example/ahorcado/ControllerMenu.java
+++ src/main/java/com/example/ahorcado/ControllerMenu.java
@@ -1,10 +1,14 @@
 package com.example.ahorcado;
 
 import javafx.event.ActionEvent;
+import Tests.Servidor;  //Importamos la clase servidor del otro paquete
+
+import java.io.IOException;
 
 public class ControllerMenu {
 
     private Escenas escena = new Escenas();
+
 
 
     public void initialize()
@@ -14,8 +18,22 @@ public class ControllerMenu {
 
 
 
-
     public void EscenaJuego(ActionEvent event) {
+        // Iniciar el servidor en un hilo separado
+        Thread serverThread = new Thread(() -> {
+            try {
+                Servidor.main(new String[]{}); // Ejecutar servidor
+            } catch (IOException e) {
+                e.printStackTrace(); // Mostrar el error en la consola
+            }
+        });
+
+        serverThread.setDaemon(true); // Permite que el servidor se cierre al cerrar la app
+        serverThread.start();
+
+        System.out.println("Servidor lanzado con éxito");
+
+        // Cargar la escena del juego
         escena.cargarEscena1(event);
     }
 }
